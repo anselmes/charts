dbc47206723c167c268b182a0d4cab445ee1e793
Schubert Anselme <schubert@anselm.es>
2026-01-19 13:32:52 -0500

    Adds selective build flags and optional upload
    Adds command-line options to build specific groups or individual charts and to control uploading. Introduces flags like --all, --upstream/--cluster/--service/--template with optional names to build subsets, and --upload to enable pushing artifacts. Makes registry configurable via an environment variable, routes template charts to a separate servicetemplate repo, skips upload by default, and hardens the packaging loop to ignore missing files. Also updates chart lists (adds msr and operator). These changes speed iteration and give CI finer control over build and publish behavior.
    
    Signed-off-by: Schubert Anselme <schubert@anselm.es>

--- build.sh
+++ build.sh
@@ -2,13 +2,24 @@
 
 set -eo pipefail
 
-BUILD_DIR=$(mktemp -d)
-REGISTRY="oci://ghcr.io/labsonline/charts"
+REGISTRY="${REGISTRY:-oci://ghcr.io/labsonline/charts}"
+UPLOAD=false
 
-UPSTREAM=(
-  rockoon
-)
+BUILD_DIR=$(mktemp -d)
+BUILD_ALL=true
+
+BUILD_CLUSTERS=false
+BUILD_SERVICES=false
+BUILD_TEMPLATES=false
+BUILD_UPSTREAM=false
+
+SPECIFIC_CLUSTERS=()
+SPECIFIC_SERVICES=()
+SPECIFIC_TEMPLATES=()
+SPECIFIC_UPSTREAM=()
+
 CLUSTERS=()
+
 SERVICES=(
   ca
   ccm
@@ -21,24 +32,103 @@ SERVICES=(
   kcm
   knative
   monitoring
+  msr
   openstack
+  operator
   pinniped
 )
+
 TEMPLATES=(
   ca-service-template
   cni-service-template
-  csi-service-template
   crd-service-template
+  csi-service-template
   edns-service-template
   gateway-service-template
   gwapi-service-template
   knative-service-template
   monitoring-service-template
+  msr-service-template
   openstack-service-template
+  operator-service-template
   pinniped-service-template
 )
 
+UPSTREAM=(
+  rockoon
+)
+
 mkdir -p "${BUILD_DIR}"
+
+# Parse command line arguments
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --all)
+      BUILD_ALL=true
+      shift
+      ;;
+    --upstream)
+      BUILD_UPSTREAM=true
+      BUILD_ALL=false
+      if [[ $# -gt 1 && ! "$2" =~ ^-- ]]; then
+        SPECIFIC_UPSTREAM+=("$2")
+        shift
+      fi
+      shift
+      ;;
+    --clusters)
+      BUILD_CLUSTERS=true
+      BUILD_ALL=false
+      shift
+      ;;
+    --cluster)
+      BUILD_CLUSTERS=true
+      BUILD_ALL=false
+      if [[ $# -gt 1 && ! "$2" =~ ^-- ]]; then
+        SPECIFIC_CLUSTERS+=("$2")
+        shift
+      fi
+      shift
+      ;;
+    --services)
+      BUILD_SERVICES=true
+      BUILD_ALL=false
+      shift
+      ;;
+    --service)
+      BUILD_SERVICES=true
+      BUILD_ALL=false
+      if [[ $# -gt 1 && ! "$2" =~ ^-- ]]; then
+        SPECIFIC_SERVICES+=("$2")
+        shift
+      fi
+      shift
+      ;;
+    --templates)
+      BUILD_TEMPLATES=true
+      BUILD_ALL=false
+      shift
+      ;;
+    --template)
+      BUILD_TEMPLATES=true
+      BUILD_ALL=false
+      if [[ $# -gt 1 && ! "$2" =~ ^-- ]]; then
+        SPECIFIC_TEMPLATES+=("$2")
+        shift
+      fi
+      shift
+      ;;
+    --upload)
+      UPLOAD=true
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      echo "Usage: $0 [--all] [--upstream [name]] [--clusters] [--cluster name] [--services] [--service name] [--templates] [--template name] [--upload]"
+      exit 1
+      ;;
+  esac
+done
 
 build() {
   local chart_path="$1"
@@ -55,39 +145,73 @@ build() {
 }
 
 # Package charts.
+if [[ "$BUILD_ALL" == true || "$BUILD_UPSTREAM" == true ]]; then
+  if [[ ${#SPECIFIC_UPSTREAM[@]} -gt 0 ]]; then
+    for chart in "${SPECIFIC_UPSTREAM[@]}"; do
+      build "upstream/${chart}" "${BUILD_DIR}"
+    done
+  elif [[ "$BUILD_ALL" == true || ${#SPECIFIC_UPSTREAM[@]} -eq 0 ]]; then
+    for chart in "${UPSTREAM[@]}"; do
+      build "upstream/${chart}" "${BUILD_DIR}"
+    done
+  fi
+fi
 
-for chart in "${UPSTREAM[@]}"; do
-  build "upstream/${chart}" "${BUILD_DIR}"
-done
+if [[ "$BUILD_ALL" == true || "$BUILD_CLUSTERS" == true ]]; then
+  if [[ ${#SPECIFIC_CLUSTERS[@]} -gt 0 ]]; then
+    for chart in "${SPECIFIC_CLUSTERS[@]}"; do
+      build "cluster/${chart}" "${BUILD_DIR}"
+    done
+  elif [[ "$BUILD_ALL" == true || ${#SPECIFIC_CLUSTERS[@]} -eq 0 ]]; then
+    for chart in "${CLUSTERS[@]}"; do
+      build "cluster/${chart}" "${BUILD_DIR}"
+    done
+  fi
+fi
 
-for cluster in "${CLUSTERS[@]}"; do
-  build "cluster/${cluster}" "${BUILD_DIR}"
-done
+if [[ "$BUILD_ALL" == true || "$BUILD_SERVICES" == true ]]; then
+  if [[ ${#SPECIFIC_SERVICES[@]} -gt 0 ]]; then
+    for chart in "${SPECIFIC_SERVICES[@]}"; do
+      build "service/${chart}" "${BUILD_DIR}"
+    done
+  elif [[ "$BUILD_ALL" == true || ${#SPECIFIC_SERVICES[@]} -eq 0 ]]; then
+    for chart in "${SERVICES[@]}"; do
+      build "service/${chart}" "${BUILD_DIR}"
+    done
+  fi
+fi
 
-for service in "${SERVICES[@]}"; do
-  build "service/${service}" "${BUILD_DIR}"
-done
-
-for template in "${TEMPLATES[@]}"; do
-  build "template/${template}" "${BUILD_DIR}"
-done
+if [[ "$BUILD_ALL" == true || "$BUILD_TEMPLATES" == true ]]; then
+  if [[ ${#SPECIFIC_TEMPLATES[@]} -gt 0 ]]; then
+    for chart in "${SPECIFIC_TEMPLATES[@]}"; do
+      build "template/${chart}" "${BUILD_DIR}"
+    done
+  elif [[ "$BUILD_ALL" == true || ${#SPECIFIC_TEMPLATES[@]} -eq 0 ]]; then
+    for chart in "${TEMPLATES[@]}"; do
+      build "template/${chart}" "${BUILD_DIR}"
+    done
+  fi
+fi
 
 # Push charts.
-for chart in "${BUILD_DIR}"/*.tgz; do
-  chart_name=$(basename "${chart}" .tgz)
+if [[ "$UPLOAD" == true ]]; then
+  for chart in "${BUILD_DIR}"/*.tgz; do
+    [[ -f "$chart" ]] || continue
+    chart_name=$(basename "${chart}" .tgz)
 
-  # Check if this is a template chart
-  is_template=false
-  for template in "${TEMPLATES[@]}"; do
-    if [[ "${chart_name}" == "${template}"* ]]; then
-      is_template=true
-      break
+    # Check if this is a template chart
+    is_template=false
+    for template in "${TEMPLATES[@]}"; do
+      if [[ "${chart_name}" == "${template}"* ]]; then
+        is_template=true
+        break
+      fi
+    done
+
+    if [[ "${is_template}" == "true" ]]; then
+      helm push "${chart}" "${REGISTRY}/servicetemplate"
+    else
+      helm push "${chart}" "${REGISTRY}"
     fi
   done
-
-  if [[ "${is_template}" == "true" ]]; then
-    helm push "${chart}" "${REGISTRY}/servicetemplate"
-  else
-    helm push "${chart}" "${REGISTRY}"
-  fi
-done
+fi
